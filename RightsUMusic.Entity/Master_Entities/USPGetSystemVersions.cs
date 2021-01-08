f3c33c9b2586b2d67b291aa5c9cd413815a4b228
suraj <suraj@SURAJ>
2021-01-08 18:28:14 +0530

    Get system version new api implementation

---
+++ RightsUMusic.Entity/Master_Entities/USPGetSystemVersions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightsUMusic.Entity
+{
+    public class USPGetSystemVersions
+    {
+        public string Version_No { get; set; }
+        public string Version_Published_Date { get; set; }
+    }
+}
