e3f0bfb1ed1ccd7899c7e0837f76eb6c8806519c
uto-rahul.kembhavi <Rahul@uto.in>
2021-01-15 15:58:24 +0530

    MusicHub enhancements

---
+++ RightsU_DB/dbo/Dev_Query/DevTagQuery.sql
@@ -0,0 +1,4 @@
+﻿IF NOT EXISTS(SELECT TOP 1 * from System_Versions where Version_No = '1.02.04')
+BEGIN
+	INSERT INTO System_Versions VALUES('1.02.04','MusicHub',GETDATE(),'First test version MusicHub')
+END
