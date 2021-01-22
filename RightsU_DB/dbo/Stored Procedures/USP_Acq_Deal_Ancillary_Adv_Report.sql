a641ecfe22e8061cc8f439c277aad8d8923b202d
uto-aditya.bandivadekar <aditya@uto.in>
2021-01-22 18:13:30 +0530

    Commit and push change for music hub and PA Reports

--- RightsU_DB/dbo/Stored Procedures/USP_Acq_Deal_Ancillary_Adv_Report.sql
+++ RightsU_DB/dbo/Stored Procedures/USP_Acq_Deal_Ancillary_Adv_Report.sql
@@ -1,7 +1,4 @@
-﻿
-
-
- CREATE PROCEDURE [dbo].[USP_Acq_Deal_Ancillary_Adv_Report]  
+﻿CREATE PROCEDURE [dbo].[USP_Acq_Deal_Ancillary_Adv_Report]  
 (  
 	@Agreement_No VARCHAR(MAX),  
 	@Title_Codes VARCHAR(MAX),  
@@ -28,11 +25,24 @@ BEGIN
   
  --SELECT  
  --@Agreement_No = '',  
- --@Title_Codes = '2138, 7511', --'545',  
+ --@Title_Codes = '', --'545',  
  --@Platform_Codes = '',--'0,1,0,3,4,0,6,7,0,0,328,329,330,331,338,339,0,341,342,343,344,345,346,347,348,349,350,351,0,332,333,334,335,352,353,0,355,356,357,358,359,360,361,362,363,364,365,0,0,366,367,0,369,370,371,372,373,374,375,376,377,378,379,0,380,381,0,383,384,385,386,387,388,389,390,391,392,393,0,0,16,17,0,20,21,0,22,23,251,24,29,181,0,252,253,254,255,0,32,33,34,0,0,0,60,258,61,62,259,63,64,65,66,67,69,0,71,72,73,0,262,263,75,76,0,145,146,265,78,79,147,152,154,0,0,38,256,39,40,257,41,42,43,44,45,47,0,49,50,51,0,260,261,53,54,0,149,150,264,56,57,151,155,157,0,0,268,269,270,271,272,273,274,275,276,277,278,0,280,281,282,0,284,285,286,287,0,291,292,293,288,289,294,295,296,0,0,299,300,301,302,303,304,305,306,307,308,309,0,311,312,313,0,315,316,317,318,0,322,323,324,319,320,325,326,327,0,0,111,112,113,182,183,184,0,114,115,116,185,186,187,0,0,173,174,175,200,201,202,0,0,120,121,122,188,189,190,0,227,228,229,230,231,232,0,123,124,125,191,192,193,0,0,165,163,167,197,195,199,0,162,166,164,194,198,196,0,213,215,214,216,218,217,0,220,222,221,223,225,224,226,0,127,128,0,336,337,130,0,248,249,250,0,132,133,134,135,0,138,139,140,141,0,234,235,236,143,0,238,239,240,241,170,0,242,243,244,180,0,207,208,209,210,0,246,247,394',  
- --@Ancillary_Type_Code = '' ,  
- --@Business_Unit_Code = 1,  
- --@IncludeExpired = 'N'  
+ --@Ancillary_Type_Code = '1,2,3,4,5,6' ,  
+ --@Business_Unit_Code = 5,  
+ --@IncludeExpired = 'Y'  
+
+ --CREATE TABLE TestParameterPA (
+ --Agreement_No VARCHAR(MAX),  
+ --Title_Codes VARCHAR(MAX),  
+ --Platform_Codes VARCHAR(MAX),  
+ --Ancillary_Type_Code VARCHAR(MAX),  
+ --Business_Unit_Code INT,  
+ --IncludeExpired VARCHAR(MAX)  
+ --)
+-- DROP TABLE TestParameterPA
+ --Select * from TestParameterPA
+ --INSERT INTO TestParameterPA(Agreement_No,Title_Codes,Platform_Codes,Ancillary_Type_Code,Business_Unit_Code,IncludeExpired)
+ --SELECT @Agreement_No,@Title_Codes,@Platform_Codes,@Ancillary_Type_Code,@Business_Unit_Code,@IncludeExpired
   
   
  IF(OBJECT_ID('tempdb..#GetDealTypeCondition') IS NOT NULL) DROP TABLE #GetDealTypeCondition  
@@ -109,8 +119,10 @@ BEGIN
   INSERT INTO @AcqDeal (Acq_Deal_Code)  
   SELECT  Acq_Deal_Code  FROM Acq_deal WHERE Deal_Workflow_Status NOT IN ('AR', 'WA') AND Business_Unit_Code = @Business_Unit_Code  
   
-  INSERT INTO @Ancillary_Deal (Acq_Deal_Ancillary_Code)  
-  SELECT Acq_Deal_Ancillary_Code FROM Acq_Deal_Ancillary WHERE Acq_deal_Code IN (SELECT Acq_Deal_Code FROM @AcqDeal)  
+  INSERT INTO @Ancillary_Deal (Acq_Deal_Ancillary_Code,Title_Code)  
+  SELECT AAD.Acq_Deal_Ancillary_Code,ADT.Title_Code FROM Acq_Deal_Ancillary AAD
+  INNER JOIN Acq_Deal_Ancillary_title ADT ON AAD.Acq_Deal_Ancillary_Code = ADT.Acq_Deal_Ancillary_Code  
+  WHERE AAD.Acq_Deal_Code IN (SELECT Acq_Deal_Code FROM @AcqDeal)  
  END  
  ELSE IF (@Title_Codes <> '' AND @Agreement_No <> '') -- Agreement No = A-2018-00129 / Title Codes = '12,13,14,5'  
  BEGIN  
@@ -168,6 +180,7 @@ BEGIN
   AND (ADR.Actual_Right_Start_Date IS NOT NULL AND ISNULL(ADR.Actual_Right_End_Date, '31DEC9999') >= GETDATE() OR @IncludeExpired = 'Y')  
   AND ( @Title_Codes = '' OR (ADRT.Title_Code  IN (SELECT Title_Code FROM @Ancillary_Deal)))  
    
+ 
   INSERT INTO #Tmp_Report
  SELECT DISTINCT  
   AD.*,  
@@ -182,10 +195,10 @@ BEGIN
   P.Platform_Code,  
   'YES' Available  
  FROM Acq_Deal_Ancillary ADA   
-  INNER JOIN @Ancillary_Deal ADN ON ADN.Acq_Deal_Ancillary_Code =  ADA.Acq_Deal_Ancillary_Code  
   INNER JOIN #Tmp_AcqDeal AD ON ADA.Acq_Deal_Code = AD.Acq_Deal_Code  
   INNER JOIN Acq_Deal_Ancillary_Platform ADP ON ADA.Acq_Deal_Ancillary_Code = ADP.Acq_Deal_Ancillary_Code  
   INNER JOIN Acq_Deal_Ancillary_title ADT ON ADT.Title_Code = AD.Title_Code  
+  INNER JOIN @Ancillary_Deal ADN ON ADN.Acq_Deal_Ancillary_Code =  ADA.Acq_Deal_Ancillary_Code  AND ADT.Title_Code = ADN.Title_Code
   INNER JOIN Platform P On P.Platform_Code = ADP.Platform_Code  
   INNER JOIN Title T On ADT.Title_Code = T.Title_Code  
   INNER JOIN Ancillary_Type AT ON AT.Ancillary_Type_Code = ADA.Ancillary_Type_Code  
