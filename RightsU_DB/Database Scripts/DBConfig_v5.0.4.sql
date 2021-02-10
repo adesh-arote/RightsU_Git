da0d99dc7ad1e96617287d9e7cb25e6cb1f04886
adesh.phone <adesh.phone@gmail.com>
2021-02-10 12:45:17 +0530

    v5.0.4 changes checkin

--- RightsU_DB/Database Scripts/DBConfig_v5.0.4.sql
+++ RightsU_DB/Database Scripts/DBConfig_v5.0.4.sql
@@ -1,3 +1,15 @@
 ﻿--///////////////// FILE DB SCHEMA v5.0.3.1
 
+IF NOT EXISTS (Select TOP 1 * from System_Right Where Right_Name = 'Download')
+BEGIN
+	Insert Into System_Right(Right_Name, Right_Code) Values('Download',168)
+END
+GO
+
+IF NOT EXISTS (Select TOP 1 * from System_Module_Right Where Module_Code = 149 AND Right_Code = 168)
+BEGIN
+	Insert Into System_Module_Right(Module_Code, Right_Code) Values(149,168)
+END
+GO
+
 --///////////////// FILE DB SCHEMA v5.0.3.2
