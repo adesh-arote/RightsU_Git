fa6be66fcd9d7f2cb1dd1c767111c38137e5038e
Prathamesh <prathamesh.naik.uto@outlook.com>
2023-03-24 12:09:43 +0530

    Party Master Changes Additions : Additional Info PopUp, with Content Rule tab, OEM, TnC

---
+++ RightsU_Entities/Get_AL_Vendor_Rule_Criteria_Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightsU_Entities
+{
+    public partial class AL_Vendor_Rule_Criteria
+    {
+        [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+        public string ExtendedColumnNames { get; set; }
+        [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+        public string DataFieldNames { get; set; }
+    }
+
+    public partial class AL_Vendor_OEM
+    {
+        [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+        public string SelectedOEMName { get; set; }
+    }
+
+    public partial class AL_Vendor_Details
+    {
+        [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+        public string SelectedPartyType { get; set; }
+    }
+}
+
+// Extra Property to display Dropdown Names
+// Not mapped to Database
