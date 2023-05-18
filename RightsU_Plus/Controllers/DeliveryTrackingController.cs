4a4da00ca39ddb0b7072e1471b8318dc3c950821
sachins <sachins@LPT59-SACHINS>
2023-05-18 16:49:44 +0530

    Search criteria , pagination, and UI changes for delivery tracking

--- RightsU_Plus/Controllers/DeliveryTrackingController.cs
+++ RightsU_Plus/Controllers/DeliveryTrackingController.cs
@@ -36,16 +36,31 @@ namespace RightsU_Plus.Controllers
         // GET: DeliveryTracking
         public ActionResult Index()
         {
+            //added by sachin shelar
+            Vendor_Service objVendor_Service = new Vendor_Service(objLoginEntity.ConnectionStringName);
+            AL_Material_Tracking_Service objAL_Material_Tracking_Service = new AL_Material_Tracking_Service(objLoginEntity.ConnectionStringName);
+            AL_Lab_Service objAL_Lab_Service = new AL_Lab_Service(objLoginEntity.ConnectionStringName);
+
+            List<RightsU_Entities.Vendor> lstClients = objVendor_Service.SearchFor(s => true).Where(w => w.Party_Type == "C").ToList();
+            ViewBag.ddlClient = new SelectList(lstClients, "Vendor_Code", "Vendor_Name");
+      
+            List<int?> lstMT = objAL_Material_Tracking_Service.SearchFor(s => true).Select(s => s.Vendor_Code).Distinct().ToList();
+            List<RightsU_Entities.Vendor> lstDistributors = objVendor_Service.SearchFor(s => true).Where(w => lstMT.Any(a => w.Vendor_Code == a)).ToList();
+            ViewBag.ddlDistributor = new SelectList(lstDistributors, "Vendor_Code", "Vendor_Name");
+          
+            List<AL_Lab> lstLab = objAL_Lab_Service.SearchFor(s => true).ToList();
+            ViewBag.ddlLab = new SelectList(lstLab, "AL_Lab_Code", "AL_Lab_Name");
+         
             return View();
         }
 
-        public JsonResult SearchDeliveryTracking(string searchText, string TabName)
+        public JsonResult SearchDeliveryTracking(string TabName = "", int Client = 0, string CycleDate = "", int Lab = 0, int Distributor = 0, string Display = "")
         {
             int recordcount = 0;
             if (TabName == "M")
             {
                 //lstGetDeliveryTrackingListMovies_Result_Searched = 
-                    GetDeliveryTrackingListMovies_Result();
+                    GetDeliveryTrackingListMovies_Result(TabName, Client, CycleDate, Lab, Distributor, Display);
 
                 recordcount = lstGetDeliveryTrackingListMovies_Result_Searched.Count -1 ;
             }
@@ -64,7 +79,7 @@ namespace RightsU_Plus.Controllers
             return Json(obj);
         }
 
-        public List<USPAL_GetDeliveryTrackingList_Result> GetDeliveryTrackingListMovies_Result()
+        public List<USPAL_GetDeliveryTrackingList_Result> GetDeliveryTrackingListMovies_Result(string TabName, int Client, string CycleDate, int Lab, int Distributor, string Display)
         {
             lstGetDeliveryTrackingListMovies_Result_Searched = new USP_Service(objLoginEntity.ConnectionStringName).USPAL_GetDeliveryTrackingList(0, "", 0, 0, "","M").ToList();
             return lstGetDeliveryTrackingListMovies_Result;
@@ -75,26 +90,45 @@ namespace RightsU_Plus.Controllers
             lstGetDeliveryTrackingListMovies_Result_Searched = new USP_Service(objLoginEntity.ConnectionStringName).USPAL_GetDeliveryTrackingList(0, "", 0, 0, "", "S").ToList();
             return lstGetDeliveryTrackingListMovies_Result;
         }
+
         public ActionResult BindMoviesList(int pageNo, int recordPerPage, string sortType, string CommandName, int id)
         {
-            List<USPAL_GetDeliveryTrackingList_Result> lst = new List<USPAL_GetDeliveryTrackingList_Result>();
+            int RecordCount = 0;
 
             ViewBag.CommandName = CommandName;
             ViewBag.AL_Material_Tracking_Code = id;
             ViewBag.TabName = CommandName;
-            int RecordCount = 0;
-            RecordCount = lstGetDeliveryTrackingListMovies_Result_Searched.Count - 1;
+
+            //added by sachin shelar
+            List<USPAL_GetDeliveryTrackingList_Result> MaterialTrackingList, lst = new List<USPAL_GetDeliveryTrackingList_Result>();
+            USPAL_GetDeliveryTrackingList_Result firstItem = new USPAL_GetDeliveryTrackingList_Result();
+            MaterialTrackingList = lstGetDeliveryTrackingListMovies_Result_Searched.ToList();
+
+            if (MaterialTrackingList.Count != 0)
+            {
+                RecordCount = MaterialTrackingList.Count - 1;
+
+                firstItem = MaterialTrackingList[0];
+                MaterialTrackingList.RemoveAt(0);
+            }
+            else
+            {
+                RecordCount = MaterialTrackingList.Count();               
+            }
+
+            //RecordCount = lstGetDeliveryTrackingListMovies_Result_Searched.Count - 1; // commented by sachin shelar
 
             if (RecordCount > 0)
             {
                 int noOfRecordSkip, noOfRecordTake;
                 pageNo = GetPaging(pageNo, recordPerPage, RecordCount, out noOfRecordSkip, out noOfRecordTake);
                 if (sortType == "T")
-                    lst = lstGetDeliveryTrackingListMovies_Result_Searched;//.OrderByDescending(o => o.Last_Updated_Time).Skip(noOfRecordSkip).Take(noOfRecordTake).ToList();
+                    lst = MaterialTrackingList.Skip(noOfRecordSkip).Take(noOfRecordTake).ToList();  // changed by sachin shelar
                 //if (sortType == "NA")
                 //    lst = lstGetDeliveryTrackingListMovies_Result_Searched.OrderBy(o => o.Booking_Sheet_No).Skip(noOfRecordSkip).Take(noOfRecordTake).ToList();
                 //if (sortType == "ND")
                 //    lst = lstGetDeliveryTrackingListMovies_Result_Searched.OrderByDescending(o => o.Booking_Sheet_No).Skip(noOfRecordSkip).Take(noOfRecordTake).ToList();
+                lst.Insert(0, firstItem); // added by sachin shelar
             }
 
             return PartialView("~/Views/DeliveryTracking/_MoviesList.cshtml", lst);
