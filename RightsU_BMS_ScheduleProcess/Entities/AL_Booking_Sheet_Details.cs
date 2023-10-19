96883816bf4a70cd0371e3846e69d9c8b777711e
uto-rahulkembhavi <rahul.kembhavi@uto.in>
2023-10-19 13:43:48 +0530

    Created new Console application for RightsU_BMS_ScheduleProcess and RDL rpt_Availability

---
+++ RightsU_BMS_ScheduleProcess/Entities/AL_Booking_Sheet_Details.cs
@@ -0,0 +1,31 @@
+﻿using Dapper.SimpleSave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightsU_LoadSheet_Export_CA.Entities
+{
+    [Table("AL_Booking_Sheet_Details")]
+    public partial class AL_Booking_Sheet_Details
+    {
+        [PrimaryKey]
+        public int AL_Booking_Sheet_Details_Code { get; set; }
+        [ForeignKeyReference(typeof(AL_Booking_Sheet))]
+        public Nullable<int> AL_Booking_Sheet_Code { get; set; }
+        public Nullable<int> Title_Code { get; set; }
+        public Nullable<int> Title_Content_Code { get; set; }
+        public Nullable<int> Extended_Group_Code { get; set; }
+        public Nullable<int> Columns_Code { get; set; }
+        public Nullable<int> Group_Control_Order { get; set; }
+        public string Validations { get; set; }
+        public string Additional_Condition { get; set; }
+        public string Display_Name { get; set; }
+        public string Allow_Import { get; set; }
+        public string Columns_Value { get; set; }
+        public string Cell_Status { get; set; }
+        public Nullable<int> Action_By { get; set; }
+        public Nullable<System.DateTime> Action_Date { get; set; }
+    }
+}
