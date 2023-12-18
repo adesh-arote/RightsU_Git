e41d432278d9bc0c772e4d1215e740ac29c9e56e
uto-adesharote <adesh.arote@uto.in>
2023-12-18 19:36:47 +0530

    Add Episode From and Episode To in the Avail report filter criteria

--- RightsU_Plus/Controllers/Availability_MovieController.cs
+++ RightsU_Plus/Controllers/Availability_MovieController.cs
@@ -1757,7 +1757,7 @@ namespace RightsU_Plus.Controllers
 
             if (Is_Enable_Modelling_Avail == "Y")
             {
-                noOfParam = 36;
+                noOfParam = 38;
             }
 
 
@@ -2026,6 +2026,8 @@ namespace RightsU_Plus.Controllers
                 parm[33] = new ReportParameter("Report_Type", Report_Type);
                 parm[34] = new ReportParameter("CreatedBy", objLoginUser.First_Name + " " + objLoginUser.Last_Name);
                 parm[35] = new ReportParameter("IncludeMetadata", chkMetaData);
+                parm[35] = new ReportParameter("EpisodeFrom", episodeFrom.ToString());
+                parm[36] = new ReportParameter("EpisodeTo", episodeTo.ToString());
 
             }
 
