9959351d1106b60d9b868f6a95e4440ca1540b10
ketanc <ketan.chavan@uto.in>
2024-01-09 18:56:25 +0530

    Add getchanel list repository

--- RightsU.BMS.DAL/Repository/MasterRepositories.cs
+++ RightsU.BMS.DAL/Repository/MasterRepositories.cs
@@ -325,6 +325,24 @@ namespace RightsU.BMS.DAL
     #region -------- Language -----------
     public class LanguageRepositories : MainRepository<Language>
     {
+        public LanguageReturn GetLanguage_List(string order, Int32 page, string search_value, Int32 size, string sort, string Date_GT, string Date_LT, Int32 id)
+        {
+            LanguageReturn objLanguageReturn = new LanguageReturn();
+
+            var param = new DynamicParameters();
+            param.Add("@order", order);
+            param.Add("@page", page);
+            param.Add("@search_value", search_value);
+            param.Add("@size", size);
+            param.Add("@sort", sort);
+            param.Add("@date_gt", Date_GT);
+            param.Add("@date_lt", Date_LT);
+            param.Add("@RecordCount", dbType: System.Data.DbType.Int64, direction: System.Data.ParameterDirection.Output);
+            param.Add("@id", id);
+            objLanguageReturn.content = base.ExecuteSQLProcedure<Language>("USPAPI_Language_List", param).ToList();
+            objLanguageReturn.paging.total = param.Get<Int64>("@RecordCount");
+            return objLanguageReturn;
+        }
         public Language Get(int Id)
         {
             var obj = new { Language_Code = Id };
