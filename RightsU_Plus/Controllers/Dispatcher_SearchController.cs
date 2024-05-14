55d253463a4c82b00406bf18b8aa4cec45dd8e52
KumarM <KumarM@LPT51-KUMAR>
2024-05-14 11:53:29 +0530

    1. Dispatcher changes has been made

--- RightsU_Plus/Controllers/Dispatcher_SearchController.cs
+++ RightsU_Plus/Controllers/Dispatcher_SearchController.cs
@@ -18,12 +18,18 @@ namespace RightsU_Plus.Controllers
         public void BindDDL()
         {
             ViewBag.EventType = new SelectList(new Email_Config_Service(objLoginEntity.ConnectionStringName).SearchFor(s => true).OrderBy(x => x.Email_Type), "Email_Config_Code", "Email_Type").ToList();
-            ViewBag.EventPlatform = new SelectList(new Event_Platform_Service(objLoginEntity.ConnectionStringName).SearchFor(s => true).OrderBy(x => x.Short_Code), "Short_Code", "Short_Code").ToList();
+            ViewBag.EventPlatform = new SelectList(new Event_Platform_Service(objLoginEntity.ConnectionStringName).SearchFor(s => true).OrderBy(x => x.Short_Code), "Event_Platform_Code", "Short_Code").ToList();
         }
 
-        public ActionResult GetNotifications(DateTime? StartDate, DateTime? EndDate, string EventType, string Platform, string MessageType, string MessageTo, string BtnType)
+        public ActionResult GetNotifications(string strStartDate, string strEndDate, string EventType, string Platform, string MessageType, string MessageTo, string BtnType)
         {
             string status = "S", message = "";
+            DateTime StartDate = new DateTime(), EndDate = new DateTime();
+            if (strStartDate != "" && strEndDate != "")
+            {
+                StartDate = Convert.ToDateTime(strStartDate);
+                EndDate = Convert.ToDateTime(strEndDate);
+            }
 
             System_Parameter_New_Service objSPNService = new System_Parameter_New_Service(objLoginEntity.ConnectionStringName);
             Notification_Service objNotificationService = new Notification_Service(objLoginEntity.ConnectionStringName);
@@ -31,30 +37,45 @@ namespace RightsU_Plus.Controllers
 
             #region Startdate Validation
             DateTime CurrentDate = DateTime.Now;
-            if (BtnType == "NA")
+            if (BtnType == "NA")   //Non archive
             {
-                System_Parameter_New objSPN = objSPNService.SearchFor(s => s.Parameter_Name == "Dispatcher_Days_Diff" && s.IsActive == "Y").FirstOrDefault();
-                DateTime CalStartDate = CurrentDate.AddDays(-(Convert.ToInt32(objSPN.Parameter_Value)));
-                if (StartDate < CalStartDate)
+                if (strStartDate != "" && strEndDate != "")
                 {
-                    status = "E";
-                    message = "Select start date above " + objSPN.Parameter_Value + " days";
-                    var obj = new
+                    System_Parameter_New objSPN = objSPNService.SearchFor(s => s.Parameter_Name == "Dispatcher_Days_Diff" && s.IsActive == "Y").FirstOrDefault();
+                    DateTime CalStartDate = CurrentDate.AddDays(-(Convert.ToInt32(objSPN.Parameter_Value)));
+                    if (StartDate < CalStartDate)
                     {
-                        Status = status,
-                        Message = message
-                    };
-                    return Json(obj);
+                        status = "E";
+                        message = "Select start date above " + objSPN.Parameter_Value + " days";
+                        var obj = new
+                        {
+                            Status = status,
+                            Message = message
+                        };
+                        return Json(obj);
+                    }
                 }
             }
-            if (BtnType == "AR")
+            if (BtnType == "AR")   //Archive
             {
                 StartDate = CurrentDate.AddDays(-10);
                 EndDate = DateTime.Now;
             }
             #endregion
 
-            lstnotification = objNotificationService.SearchFor(s => true).ToList().Where(a => (a.CreatedOn >= StartDate && a.CreatedOn <= EndDate)).ToList();
+            lstnotification = objNotificationService.SearchFor(s => true).ToList();
+
+            if (BtnType == "NA")
+                if (strStartDate != "" && strEndDate != "")
+                    lstnotification = lstnotification.Where(a => (a.CreatedOn >= StartDate && a.CreatedOn <= EndDate)).ToList();
+            if (BtnType == "AR")
+                lstnotification = lstnotification.Where(a => (a.CreatedOn >= StartDate && a.CreatedOn <= EndDate)).ToList();
+            if (EventType != null && EventType != "")
+                lstnotification = lstnotification.Where(a => a.Email_Config_Code == Convert.ToInt32(EventType)).ToList();
+            if (Platform != null && Platform != "")
+                lstnotification = lstnotification.Where(a => a.Event_Platform_Code == Convert.ToInt32(Platform)).ToList();            
+            if (MessageTo != null && MessageTo != "")
+                lstnotification = lstnotification.Where(a => a.Email.Contains(MessageTo)).ToList();
             if (MessageType != null)
             {
                 ViewBag.MessageType = MessageType;
@@ -63,8 +84,6 @@ namespace RightsU_Plus.Controllers
                 if (MessageType == "TEXT")
                     lstnotification = lstnotification.Where(a => a.TextBody != null).ToList();
             }
-            if (MessageTo != null && MessageTo != "")
-                lstnotification = lstnotification.Where(a => a.Email.Contains(MessageTo)).ToList();
 
             return PartialView("_Dispatcher_List", lstnotification);
         }
@@ -74,43 +93,34 @@ namespace RightsU_Plus.Controllers
         {
             try
             {
-                string status = "", message = "";
                 Notification_Service objNotificationService = new Notification_Service(objLoginEntity.ConnectionStringName);
                 Notifications Objnotifications = new Notifications();
-                Objnotifications = objNotificationService.SearchFor(x=> true).Where(x=>x.NotificationsCode == NotificationCode).FirstOrDefault();
+                Objnotifications = objNotificationService.SearchFor(x => true).Where(x => x.NotificationsCode == NotificationCode).FirstOrDefault();
 
+                ViewBag.EventType = Objnotifications.Email_Config.Email_Type;
                 if (MsgType == "HTML")
                 {
                     if (Objnotifications.HtmlBody != null)
-                    {                        
+                    {
                         ViewBag.Html = new HtmlString(Objnotifications.HtmlBody);
-                        return PartialView("_MessageViewPopUp");
                     }
                     else
-                    {
-                        status = "E";
-                        message = "There is no Message for this event";
+                    {                       
+                        ViewBag.Html = "There is no Message for this event";
                     }
                 }
                 if (MsgType == "TEXT")
                 {
                     if (Objnotifications.HtmlBody != null)
                     {
-                        ViewBag.Html = new HtmlString(Objnotifications.TextBody);
-                        return PartialView("_MessageViewPopUp");
+                        ViewBag.Html = new HtmlString(Objnotifications.TextBody);                       
                     }
                     else
                     {
-                        status = "E";
-                        message = "There is no Message for this event";
+                        ViewBag.Html = "There is no Message for this event";
                     }
                 }
-                var obj = new
-                {
-                    Status = status,
-                    Message = message
-                };
-                return Json(obj);
+                return PartialView("_MessageViewPopUp");
             }
             catch (Exception ex)
             {
