392382d9ea7da7e80d039086188d35cc6b946b00
KumarM <KumarM@LPT51-KUMAR>
2024-05-22 16:02:53 +0530

    Dispatcher Search Screen Check in

--- RightsU_Plus/Controllers/BaseController.cs
+++ RightsU_Plus/Controllers/BaseController.cs
@@ -10,6 +10,11 @@ using RightsU_Entities;
 using UTOFrameWork.FrameworkClasses;
 using System.Data.Entity.Core.Objects;
 using System.Reflection;
+using System.Net;
+using System.Text;
+using System.Configuration;
+using System.IO;
+using System.Security.Cryptography;
 //using UTOFrameWork.FrameworkClasses;
 
 namespace RightsU_Plus.Controllers
@@ -1755,5 +1760,49 @@ namespace RightsU_Plus.Controllers
             return Json(obj);
         }
 
+        public string GetAuthKey()
+        {
+            string AuthKey = "";
+            string hostName = Dns.GetHostName(); // Retrive the Name of HOST
+            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+
+            byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings["salt"].ToString());
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(myIP);
+
+            byte[] bytesEncrypted = AesOperation.AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+            AuthKey = Convert.ToBase64String(bytesEncrypted);
+
+            return AuthKey;
+        }
+        class AesOperation
+        {
+            public static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+            {
+                byte[] encryptedBytes = null;
+                byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (RijndaelManaged AES = new RijndaelManaged())
+                    {
+                        AES.KeySize = 256;
+                        AES.BlockSize = 128;
+
+                        var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                        AES.Key = key.GetBytes(AES.KeySize / 8);
+                        AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                        AES.Mode = CipherMode.CBC;
+
+                        using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                            cs.Close();
+                        }
+                        encryptedBytes = ms.ToArray();
+                    }
+                }
+                return encryptedBytes;
+            }
+        }
     }
 }
